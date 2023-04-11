4ad804c54704bfe344573d78142490f0c65a22dc
Henrique Terres <henrique.terres00@edu.pucrs.br>
2023-04-10 23:43:45 -0300

    First commit with routes

---
+++ professors/src/main/java/br/pucrs/csw/professors/ProfessorsApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.csw.professors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorsApplication.class, args);
+	}
+
+}
---
+++ professors/src/test/java/br/pucrs/csw/professors/ProfessorsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.csw.professors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f2b148d1c86c60934a25d480daaa8d8e77e47ea4
Jp <joao@ubots.com.br>
2023-05-21 19:23:55 -0300

    iniciando swagger

--- src/main/java/br/csw/opensarc/professors/ProfessorsApplication.java
+++ src/main/java/br/csw/opensarc/professors/ProfessorsApplication.java
@@ -1,9 +1,13 @@
 package br.csw.opensarc.professors;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Sistema de gerenciamento de professores", version = "1.0.0"))
 public class ProfessorsApplication {
 
 	public static void main(String[] args) {

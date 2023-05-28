17adef3f3fd4b29c896094261a9147fe8b273333
Jp <joao@ubots.com.br>
2023-05-28 19:38:33 -0300

    esqueci de ti

--- src/main/java/br/csw/opensarc/professors/repository/mapper/ProfessorCertificationRowMapper.java
+++ src/main/java/br/csw/opensarc/professors/repository/mapper/ProfessorCertificationRowMapper.java
@@ -9,6 +9,14 @@ import java.sql.SQLException;
 public class ProfessorCertificationRowMapper implements RowMapper<ProfessorCertificationEntity> {
     @Override
     public ProfessorCertificationEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        String id = rs.getString("id");
+        String professor_id = rs.getString("professor_id");
+        String certification_id = rs.getString("certification_id");
+        String level = rs.getString("level");
+        String name = rs.getString("name");
+        String institution = rs.getString("institution");
+        String year = Integer.toString(rs.getDate("year").toLocalDate().getYear());
+        String semester = rs.getString("semester");
+        return new ProfessorCertificationEntity(id, professor_id, certification_id, level, name, institution, year, semester);
     }
 }

99a03002a8c1071e217d5d35a7de72c98f9ec436
Jp <joao@ubots.com.br>
2023-05-28 19:40:51 -0300

    config

--- src/main/java/br/csw/opensarc/professors/config/ProfessorsConfig.java
+++ src/main/java/br/csw/opensarc/professors/config/ProfessorsConfig.java
@@ -1,11 +1,14 @@
 package br.csw.opensarc.professors.config;
 
 import br.csw.opensarc.professors.controller.CertificationController;
+import br.csw.opensarc.professors.controller.ProfessorCertificationController;
 import br.csw.opensarc.professors.controller.ProfessorController;
 import br.csw.opensarc.professors.repository.CertificationRepository;
 import br.csw.opensarc.professors.repository.IdentificationsRepository;
+import br.csw.opensarc.professors.repository.ProfessorCertificationRepository;
 import br.csw.opensarc.professors.repository.ProfessorRepository;
 import br.csw.opensarc.professors.service.CertificatioService;
+import br.csw.opensarc.professors.service.ProfessorCertificationService;
 import br.csw.opensarc.professors.service.ProfessorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +23,11 @@ public class ProfessorsConfig {
     }
 
     @Bean
+    public ProfessorCertificationController professorCertificationController(ProfessorCertificationService service) {
+        return new ProfessorCertificationController(service);
+    }
+
+    @Bean
     public ProfessorController professorController(ProfessorService professorService) {
         return new ProfessorController(professorService);
     }
@@ -30,6 +38,13 @@ public class ProfessorsConfig {
     }
 
     @Bean
+    public ProfessorCertificationService professorCertificationService(ProfessorCertificationRepository repository,
+                                                                       ProfessorRepository professorRepository,
+                                                                       CertificationRepository certificationRepository) {
+        return new ProfessorCertificationService(repository, certificationRepository, professorRepository);
+    }
+
+    @Bean
     public CertificatioService certificationService(CertificationRepository certificationRepository) {
         return new CertificatioService(certificationRepository);
     }
@@ -37,6 +52,11 @@ public class ProfessorsConfig {
     @Bean
     public ProfessorRepository professorRepository(NamedParameterJdbcTemplate jdbcTemplate) {
         return new ProfessorRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public ProfessorCertificationRepository professorCertificationRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        return new ProfessorCertificationRepository(jdbcTemplate);
     }
 
     @Bean

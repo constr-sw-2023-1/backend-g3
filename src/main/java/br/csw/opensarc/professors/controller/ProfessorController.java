fe39452b88ccf756e3dfd98cd69b6bf2ca751132
Jp <joao@ubots.com.br>
2023-05-29 19:24:09 -0300

    ajuste parameters

--- src/main/java/br/csw/opensarc/professors/controller/ProfessorController.java
+++ src/main/java/br/csw/opensarc/professors/controller/ProfessorController.java
@@ -8,6 +8,8 @@ import br.csw.opensarc.professors.model.Professor;
 import br.csw.opensarc.professors.service.ProfessorService;
 import br.csw.opensarc.professors.service.exception.InsertError;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -50,10 +52,28 @@ public class ProfessorController {
     }
 
     @GetMapping
-    @Operation(summary = "Get all professors")
-    @ApiResponse(responseCode = "200", description = "OK")
+    @Operation(summary = "Get all professors",
+            description = "Used to filter the professor can be passed expanded passing queryParameters with the following pattern\n" +
+                    "Condição\tOperador " +
+                    "{op}\tExemplo\n" +
+                    "Equals\t\tstatus=Liberado\n" +
+                    "Not equals\tneq\tstatus={neq}Reservado\n" +
+                    "Greater than\tgt\tvalue={gt}20\n" +
+                    "Greater than equals\tgteq\tinitial_date={gteq}2022-11-08\n" +
+                    "Less than\tlt\tfinal_date={lt}2022-11-30\n" +
+                    "Less than equals\tlteq\tquantity={lteq}2.5\n" +
+                    "Like\tlike\tdescription={like}%database%"
+    )
+    @ApiResponse(responseCode = "200", description = "OK",
+    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = Professor.class)))
+    )
+    @Parameter(name ="id", description = "Query filters to Filter the id", in = ParameterIn.QUERY)
+    @Parameter(name ="registration", description = "Query filters to Filter the registration", in = ParameterIn.QUERY)
+    @Parameter(name ="name", description = "Query filters to Filter the name", in = ParameterIn.QUERY)
+    @Parameter(name ="bornDate", description = "Query filters to Filter the bornDate", in = ParameterIn.QUERY)
+    @Parameter(name ="admissionDate", description = "Query filters to Filter the admissionDate", in = ParameterIn.QUERY)
+    @Parameter(name ="active", description = "Query filters to Filter the active", in = ParameterIn.QUERY)
     public ResponseEntity<List<Professor>> getAll(
-            @Parameter(description = "Query filters to apply", example = "name=Arthur&department=Math")
             @RequestParam(required = false) Map<String, String> filters) {
         return ResponseEntity.ok(professorService.getAll(filters));
     }

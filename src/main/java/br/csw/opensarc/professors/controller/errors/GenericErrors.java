55d38e8598d695ba3713de5ca2f43d2dc58430da
Jp <joao@ubots.com.br>
2023-05-31 11:10:32 -0300

    tratando erros

--- src/main/java/br/csw/opensarc/professors/controller/errors/GenericErrors.java
+++ src/main/java/br/csw/opensarc/professors/controller/errors/GenericErrors.java
@@ -1,7 +1,9 @@
 package br.csw.opensarc.professors.controller.errors;
 
 import br.csw.opensarc.professors.controller.dto.ErrorMessage;
+import br.csw.opensarc.professors.service.exception.CertificationNotFoundException;
 import br.csw.opensarc.professors.service.exception.InsertError;
+import br.csw.opensarc.professors.service.exception.UpdateError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,6 +19,22 @@ public class GenericErrors {
             return new ResponseEntity<>(new ErrorMessage(ex.getFieldError().getDefaultMessage(), "USR-02"), HttpStatus.BAD_REQUEST);
         }
         return  new ResponseEntity<>(new ErrorMessage(ex.getMessage(), "USR-03"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UpdateError.class)
+    public ResponseEntity<ErrorMessage> handelUpdateError(UpdateError exception) {
+        return new ResponseEntity<>(
+                new ErrorMessage(exception.getMessage(), "POST-02"),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(CertificationNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handelCertificationNotFound(CertificationNotFoundException exception) {
+        return new ResponseEntity<>(
+                new ErrorMessage(exception.getMessage(), "USR-04"),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
     }
 
     @ExceptionHandler(InsertError.class)
